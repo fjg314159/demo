7c52a3c1bce92f3270cf4cf9e78f006e53655444
Fan <2952501287@qq.com>
2022-11-28 20:22:52 +0800

    thrid   commit

--- src/main/java/com/fan/gittest/Hello.java
+++ src/main/java/com/fan/gittest/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("你好,git");
         System.out.println("hello,java");
-
+        System.out.println("hello,github");
     }
 }
