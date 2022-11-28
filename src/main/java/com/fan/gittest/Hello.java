85e683ea2838c0d4c61c4e45f047ff007eeb8d1e
Fan <2952501287@qq.com>
2022-11-28 20:22:27 +0800

    second   commit

--- src/main/java/com/fan/gittest/Hello.java
+++ src/main/java/com/fan/gittest/Hello.java
@@ -3,5 +3,7 @@ package com.fan.gittest;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("你好,git");
+        System.out.println("hello,java");
+
     }
 }
